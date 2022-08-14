c01b5c7c1c3a8bcc6eebcb15ad5849234295efc2
thieulong <thieulongph@gmail.com>
2022-08-14 22:55:08 +1000

    Done

---
+++ make_data.py
@@ -0,0 +1,51 @@
+import cv2
+import mediapipe as mp
+import pandas as pd
+
+cap = cv2.VideoCapture(0)
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+
+lm_list = []
+label = "punch"
+no_of_frames = 600
+i=0
+
+def make_landmark_timestep(results):
+    print(results.pose_landmarks.landmark)
+    c_lm = []
+    for id, lm in enumerate(results.pose_landmarks.landmark):
+        c_lm.append(lm.x)
+        c_lm.append(lm.y)
+        c_lm.append(lm.z)
+        c_lm.append(lm.visibility)
+    return c_lm
+
+def draw_landmark_on_image(mpDraw, results, frame):
+    mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+    for id, lm in enumerate(results.pose_landmarks.landmark):
+        h, w, c = frame.shape
+        print(id, lm)
+        cx, cy = int(lm.x * w), int(lm.y * h)
+        cv2.circle(frame, (cx, cy), 3, (0, 255, 0), cv2.FILLED)
+    return frame
+
+while len(lm_list)<=no_of_frames:
+    ret, frame = cap.read()
+    if ret:
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(frameRGB)
+        if results.pose_landmarks:
+            lm = make_landmark_timestep(results)
+            lm_list.append(lm)
+            frame = draw_landmark_on_image(mpDraw, results, frame)
+        cv2.imshow("image", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+df = pd.DataFrame(lm_list)
+df.to_csv(label+".txt")
+cap.release()
+cv2.destroyAllWindows()
---
+++ train_lstm.py
@@ -0,0 +1,48 @@
+from typing import Sequence
+import numpy as np
+import pandas as pd
+from keras.layers import LSTM, Dense, Dropout
+from keras.models import Sequential
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+punch_df = pd.read_csv("punch.txt")
+neutral_df = pd.read_csv("neutral.txt")
+
+X = []
+y = []
+no_of_timesteps = 20q
+
+datasets = punch_df.iloc[:,1:].values
+n_samples = len(datasets)
+
+for i in range(no_of_timesteps, n_samples):
+    X.append(datasets[i-no_of_timesteps:i,:])
+    y.append(1)
+
+datasets = neutral_df.iloc[:,1:].values
+n_samples = len(datasets)
+
+for i in range(no_of_timesteps, n_samples):
+    X.append(datasets[i-no_of_timesteps:i,:])
+    y.append(0)
+
+X, y = np.array(X), np.array(y)
+print(X.shape, y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1, activation="sigmoid"))
+model.compile(optimizer="adam", metrics=["accuracy"],loss="binary_crossentropy")
+
+model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
+model.save("lstm-model.h5")
